88d29b5a961c7eae4ab1569ae7d8d679c14f1431
Hasitha <hasithawijewardhana19@gmail.com>
2025-03-16 02:08:39 +0530

    create the contact us page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './Pages/Home Page/HomePage';
 import AboutusPage from './Pages/Aboutus Page/AboutusPage';
 import Members from './Pages/Members/Members';
+import ContactUs from './Pages/Contact_us/Contact_us';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutusPage />} />
           <Route path="/members" element={<Members/>} />
+          <Route path="/contactus" element={<ContactUs/>} />
         </Routes>
       </BrowserRouter>
 
